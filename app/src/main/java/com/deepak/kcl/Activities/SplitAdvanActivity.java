e7bf519714169baff4abf9500922b3d70b8e68cd
Deepak <dayadeepak03@gmail.com>
2019-06-08 17:03:20 +0530

    Add Expense Dialog with splitAdvanceActivity and ExpenseFragment.

--- app/src/main/java/com/deepak/kcl/Activities/SplitAdvanActivity.java
+++ app/src/main/java/com/deepak/kcl/Activities/SplitAdvanActivity.java
@@ -1,15 +1,37 @@
 package com.deepak.kcl.Activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.deepak.kcl.R;
+
+import java.util.Calendar;
 
 public class SplitAdvanActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    ImageButton imgbtnAdd;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +41,7 @@ public class SplitAdvanActivity extends AppCompatActivity {
 
     private void initView() {
         toolbar = findViewById(R.id.splitAdvanceToolbar);
+        imgbtnAdd = findViewById(R.id.split_adv_imgBtnAdd);
         initializeView();
     }
 
@@ -27,6 +50,96 @@ public class SplitAdvanActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+
+        imgbtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenAddExpenseDialog();
+            }
+        });
+    }
+
+    private void OpenAddExpenseDialog() {
+        Button btnSave,btnCancel;
+        ImageButton imgBtnClose,imgBtnDate;
+        Spinner spnBranch,spnSplit;
+        EditText edtDate;
+        TextView txtHeader;
+
+        final Dialog dialog=new Dialog(SplitAdvanActivity.this);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_expense);
+        if (dialog.getWindow()!=null)
+        {
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
+            WindowManager.LayoutParams params=dialog.getWindow().getAttributes();
+            params.gravity= Gravity.CENTER_VERTICAL;
+        }
+
+        imgBtnClose = dialog.findViewById(R.id.dialog_exp_imgBtnClose);
+        btnSave = dialog.findViewById(R.id.dialog_exp_btnSave);
+        btnCancel = dialog.findViewById(R.id.dialog_exp_btnCancel);
+        spnBranch = dialog.findViewById(R.id.dialog_exp_spnBranch);
+        spnSplit = dialog.findViewById(R.id.dialog_exp_spnSplit);
+        imgBtnDate = dialog.findViewById(R.id.dialog_exp_imgBtnDate);
+        edtDate = dialog.findViewById(R.id.dialog_exp_edtDate);
+        txtHeader = dialog.findViewById(R.id.dialog_exp_header);
+
+        txtHeader.setText("Split Advance");
+
+        edtDate.setKeyListener(null);
+
+        imgBtnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(SplitAdvanActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                edtDate.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.Branches, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnBranch.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
+                this, R.array.SplitType, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSplit.setAdapter(adapter1);
+
+        imgBtnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
 
     @Override
--- app/src/main/java/com/deepak/kcl/Fragments/ExpenseFragment.java
+++ app/src/main/java/com/deepak/kcl/Fragments/ExpenseFragment.java
@@ -1,13 +1,28 @@
 package com.deepak.kcl.Fragments;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.deepak.kcl.R;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +30,12 @@ import com.deepak.kcl.R;
 public class ExpenseFragment extends Fragment {
 
     View view;
+    ImageButton imgBtnAdd;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
     public ExpenseFragment() {
         // Required empty public constructor
     }
@@ -25,7 +46,103 @@ public class ExpenseFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_expense, container, false);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        imgBtnAdd = view.findViewById(R.id.exp_imgbtn_add);
+        initializeView();
+    }
+
+    private void initializeView() {
+
+        imgBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenAddExpenseDialog();
+            }
+        });
+    }
+
+    private void OpenAddExpenseDialog() {
+        Button btnSave,btnCancel;
+        ImageButton imgBtnClose,imgBtnDate;
+        Spinner spnBranch,spnSplit;
+        EditText edtDate;
+
+
+        final Dialog dialog=new Dialog(getActivity());
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_expense);
+        if (dialog.getWindow()!=null)
+        {
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
+            WindowManager.LayoutParams params=dialog.getWindow().getAttributes();
+            params.gravity= Gravity.CENTER_VERTICAL;
+        }
+
+        imgBtnClose = dialog.findViewById(R.id.dialog_exp_imgBtnClose);
+        btnSave = dialog.findViewById(R.id.dialog_exp_btnSave);
+        btnCancel = dialog.findViewById(R.id.dialog_exp_btnCancel);
+        spnBranch = dialog.findViewById(R.id.dialog_exp_spnBranch);
+        spnSplit = dialog.findViewById(R.id.dialog_exp_spnSplit);
+        imgBtnDate = dialog.findViewById(R.id.dialog_exp_imgBtnDate);
+        edtDate = dialog.findViewById(R.id.dialog_exp_edtDate);
+
+        edtDate.setKeyListener(null);
+
+        imgBtnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                edtDate.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getActivity(), R.array.Branches, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnBranch.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
+                getActivity(), R.array.SplitType, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSplit.setAdapter(adapter1);
+
+        imgBtnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
 
 }
